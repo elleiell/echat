0b24990abba4f192f6f96d319168ae8a689ec488
Elleiell Balisi <elleiellb@gmail.com>
2024-02-02 18:41:39 +0800

    Fixing vertical alignment

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,24 +4,26 @@ import EChatLogo from '../images/chat-logo.png'
 
 function Welcome() {
     return(
-        <div className='welcome'>
-            <div className='logo'>
-                <img src={EChatLogo} alt='EChat Logo' />
-                <h1>E<span className='orange'>Chat</span></h1>
-            </div>
+        <div className='container'>
+            <div className='welcome'>
+                <div className='logo'>
+                    <img src={EChatLogo} alt='EChat Logo' />
+                    <h1>E<span className='orange'>Chat</span></h1>
+                </div>
 
-            <div className='welcome-img'>
-                <img src={ChatImg} alt="Welcome to EChat" />
-            </div>
-            
-            <h2>Welcome to EChat!</h2>
-            <p>Our chat app is the perfect wat to stay connected with friends.</p>
-            
-            <div className='signin-form'>
-                <form>
-                    <button className='btn btn-lightorange'>Sign in</button>
-                    <button className='btn btn-darkorange'>Sign up</button>
-                </form>
+                <div className='welcome-img'>
+                    <img src={ChatImg} alt="Welcome to EChat" />
+                </div>
+                
+                <h2>Welcome to EChat!</h2>
+                <p>Our chat app is the perfect wat to stay connected with friends.</p>
+                
+                <div className='signin-form'>
+                    <form>
+                        <button className='btn btn-lightorange'>Sign in</button>
+                        <button className='btn btn-darkorange'>Sign up</button>
+                    </form>
+                </div>
             </div>
         </div>
     )
