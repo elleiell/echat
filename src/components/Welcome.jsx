f2ff17aeab57c2d07c0f9a7ce36607ba3136b6db
Elleiell Balisi <elleiellb@gmail.com>
2024-02-05 13:12:42 +0800

    CSS to Tailwind

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,31 +1,40 @@
-import './Welcome.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import ChatImg from '../images/chatting.jpg'
 import EChatLogo from '../images/chat-logo.png'
-import './output.css'
+
 
 function Welcome() {
+    const chatTitle = "EChat App";
+    const userIcon = <FontAwesomeIcon icon="fa-regular fa-user" />
     return(
-        <div className='container'>
-            <div className='welcome'>
-                <div className='logo'>
-                    <img src={EChatLogo} alt='EChat Logo' />
-                    <h1>E<span className='orange'>Chat</span></h1>
+        <div className='bg-peach'>
+            <div className='container mx-auto h-screen w-full flex flex-wrap justify-center p-0 m-0'>
+                <div className='flex flex-wrap justify-center justify-items-center items-center aligh-items self-center pt-6'>
+                    <div className='shrink-0'>
+                        <img className='w-16 md:w-13 h-auto' src={EChatLogo} alt={chatTitle} />
+                    </div>
+                    <h1 className='text-6xl font-bold text-black antialiased font-sans'>e<span className='text-red-500'>Chat</span></h1>
                 </div>
 
-                <div className='welcome-img'>
-                    <img src={ChatImg} alt="Welcome to EChat" />
+                <div className='px-4 md:px-1'>
+                    <img src={ChatImg} alt={chatTitle} className='w-full md:w-1/3 h-auto m-auto' />
+                    <h2 className='text-3xl text-center font-bold font-sans mt-2'>Welcome to our <span className='text-4xl font-bold text-black antialiased font-sans'>e<span className='text-red-500'>Chat</span></span>!</h2>
+                    <p className='text-center font-sans mt-2'>Our chat app is the perfect way to be connected privately with your love ones.</p>
                 </div>
-                
-                <h2 className='text-3xl font-bold underline text-center text-red-500'>Welcome to EChat!</h2>
-                <p>Our chat app is the perfect wat to stay connected with friends.</p>
-                
-                <div className='signin-form'>
-                    <form>
-                        <button className='btn btn-lightorange'>Sign in</button>
-                        <button className='btn btn-darkorange'>Sign up</button>
-                    </form>
+
+                <div className='text-center'>
+                    <button class="bg-orange-400 hover:bg-orange-500 text-white px-28 md:px-24 py-5 mx-2 my-2 text-xl font-semibold rounded-lg">
+                        Sign in
+                    </button>
+                    <button class="bg-red-500 hover:bg-red-600 text-white px-28 md:px-24 py-5 mx-2 my-2 text-xl font-semibold rounded-lg">
+                    {userIcon} Sign up
+                    </button>
                 </div>
             </div>
+
+            <footer>
+                <p>Copyright </p>
+            </footer>
         </div>
     )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: 'Oswald, ui-serif', 
+      },
+      backgroundColor: {
+        'peach' : '#f3e8e3',
+      },
+    },
   },
   plugins: [],
 }
